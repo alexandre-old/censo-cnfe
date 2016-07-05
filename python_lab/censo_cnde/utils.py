9c53ff60ff5cb5c586d2289052e78f6220ad76c7
Alexandre <0x41e@protonmail.ch>
2016-07-05 14:36:14 -0300

    Try again

--- python_lab/censo_cnde/utils.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import datetime as dt
-
-
-def horario():
-    return dt.datetime.now().time()
--- racket_lab/layout.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-;; Representa uma linha no arquivo .txt de acordo com o layout especificado pelo IBGE
-
-;; uma coluna representa um item na linha. Cada coluna tem uma posição inicial
-;; e um tamanho. Algumas colunas tem  valores especificos que são definidos
-;; no item categoria.
-(define-struct coluna (posicao-inicial tamanho [categoria #:auto])
-  #:auto-value '()

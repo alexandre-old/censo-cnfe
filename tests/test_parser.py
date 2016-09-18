0fcec1727352904ea165a375643c88b2fc149081
Alexandre <0x41e@protonmail.ch>
2016-09-17 21:30:44 -0300

    Adding more tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -19,6 +19,8 @@ def test_get_layout_json():
 
     layout_json = parser.get_layout_json()
 
+    assert isinstance(layout_json, dict)
+
     assert layout_json != {}
 
     expected_item_keys = ['categorias', 'posicao_inicial', 'tamanho']
@@ -45,3 +47,93 @@ def test_get_layout_json():
             # Os itens "simples" contém as chaves esperadas e apenas elas.
             assert sorted(value.keys()) == expected_item_keys
             assert len(value.keys()) == 3
+
+
+def test_layout_without_depth():
+    """Testar Layout() com um layout alternativo sem profundidade
+
+    """
+
+    layout_json = {
+        'cep': {
+            'posicao_inicial': 1,
+            'tamanho': 8,
+            'categorias': {}
+        }
+    }
+
+    layout = parser.Layout(layout=layout_json)
+
+    assert layout._keys == ['cep']
+
+    assert layout._layout == layout_json
+
+
+def test_layout_with_depth():
+    """Testar Layout() com dois níveis de profundidade. (versão do layout.json)
+
+    """
+
+    layout_json = {
+        'cep': {
+            'posicao_inicial': 1,
+            'tamanho': 8,
+            'categorias': {}
+        },
+        'complemento': {
+            'elemento_1': {
+                'posicao_inicial': 9,
+                'tamanho': 2,
+                'categorias': {}
+            },
+            'valor_1': {
+                'posicao_inicial': 11,
+                'tamanho': 2,
+                'categorias': {}
+            },
+        },
+    }
+
+    layout = parser.Layout(layout=layout_json)
+
+    expected_keys = ['cep', 'complemento__elemento_1', 'complemento__valor_1']
+
+    assert sorted(layout._keys) == expected_keys
+
+    assert isinstance(layout._layout['complemento'], parser.Layout)
+
+
+def test_layout_getitem_with_depth():
+    """Testar o método __getitem__ quando o layout.json tem profundidade.
+
+    """
+
+    layout_json = {
+        'cep': {
+            'posicao_inicial': 1,
+            'tamanho': 8,
+            'categorias': {}
+        },
+        'complemento': {
+            'elemento_1': {
+                'posicao_inicial': 9,
+                'tamanho': 2,
+                'categorias': {}
+            },
+            'valor_1': {
+                'posicao_inicial': 11,
+                'tamanho': 2,
+                'categorias': {}
+            },
+        },
+    }
+
+    layout = parser.Layout(layout=layout_json)
+
+    assert layout['cep'] == slice(0, 7)
+
+    assert isinstance(layout._layout['complemento'], parser.Layout)
+
+    assert layout._layout['complemento']['elemento_1'] == slice(8, 9)
+
+    assert layout._layout['complemento']['valor_1'] == slice(10, 11)
